ce6a4dab9ea2eedc16227ebe59c6d0978f2b2aa2
Bagus Triyanto <44406611+bagusisgood@users.noreply.github.com>
2020-07-18 13:17:51 +0700

    Initial Commit

---
+++ CoreDataTechProj/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataTechProj
+//
+//  Created by Bagus Triyanto on 18/07/20.
+//  Copyright © 2020 BagusIsGood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
